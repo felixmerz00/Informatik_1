611367fa2f80d93b9db728fc47e1dd1aee03c32e
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-10-16 13:05:38 +0200

    Score: 2/2

--- Exercise_3/Task2/public/script.py
+++ Exercise_3/Task2/public/script.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 
-pwd = "abc"
+pwd = "xW+5xW+5"
 
 def is_valid():
     # You need to change the following part of the function
     # to determine if it is a valid password.
     validity = True
+
+    # 1 Has a length of 8-16 chars.
+    if (len(pwd) < 8 or len(pwd) > 16):
+        validity = False
+
+    # 2 Only contains the characters a-z, A-Z, digits, 
+    # or the special chars "+", "-", "*", "/".
+    lower_chars = "abcdefghijklmnopqrstuvwxyzöüä"
+    upper_chars = lower_chars.upper()
+    digits = "0123456789"
+    allowed_symbols = "+-*/"
+    all_chars = lower_chars+upper_chars+digits+allowed_symbols
+    for c in pwd:
+        if(not c in all_chars):
+            validity = False
+    
+    # 3 Must contain at least 2 lower case and 2 upper case characters, 
+    # 2 digits, and 2 special chars. 
+    count = [0, 0, 0, 0]    # [lower case, upper case, digits, special chars]
+    for c in pwd:
+        if c in lower_chars:
+            count[0] += 1
+        elif c in upper_chars:
+            count[1] += 1
+        elif c in digits:
+            count[2] += 1
+        elif c in allowed_symbols:
+            count[3] += 1
+    for i in count:
+        if i < 2:
+            validity = False
 
     # You don't need to change the following line.
     return validity
--- Exercise_3/Task2/public/tests.py
+++ Exercise_3/Task2/public/tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from unittest import TestCase
 
-from public import script
+import script
 
 # This test suite only tests whether the value returned by
 # the solution function has the correct type. If this test
@@ -18,3 +18,67 @@ class PublicTestSuite(TestCase):
         m = "The is_valid function should return a bool!"
         self.assertTrue(isinstance(actual, bool), m)
 
+    def test_length(self):
+        script.pwd = "HelloHe"  # 7 chars
+        res = script.is_valid()
+        self.assertEqual(res, False)
+
+        script.pwd = "HEll00++"   # 8 chars
+        res = script.is_valid()
+        self.assertEqual(res, True)
+
+        script.pwd = "HEll00++loHelloH"    # 16 chars
+        res = script.is_valid()
+        self.assertEqual(res, True)
+
+        script.pwd = "HEll00++loHelloHe"    # 17 chars
+        res = script.is_valid()
+        self.assertEqual(res, False)
+    
+    def test_correct_characters(self):
+        script.pwd = "HEll00++löHällüH"    # Correct symbols
+        res = script.is_valid()
+        self.assertEqual(res, True)
+        
+        script.pwd = "HEll00+.loHelloH"    # False symbol "."
+        res = script.is_valid()
+        self.assertEqual(res, False)
+
+        script.pwd = "HEll00++=oHelloH"    # False symbol "="
+        res = script.is_valid()
+        self.assertEqual(res, False)
+
+    def test_correct_number_of_chars(self):
+        script.pwd = "ÖÄül00++"    # Correct symbols
+        res = script.is_valid()
+        self.assertEqual(res, True)
+
+        script.pwd = "ÖÄTl00++"    # Only one lower case char
+        res = script.is_valid()
+        self.assertEqual(res, False)
+
+        script.pwd = "Öüül00++"    # Only one upper case char
+        res = script.is_valid()
+        self.assertEqual(res, False)
+
+        script.pwd = "ÖÄül0e++"    # Only one digit
+        res = script.is_valid()
+        self.assertEqual(res, False)
+        
+        script.pwd = "ÖÄül00p+"    # Only one special symbol
+        res = script.is_valid()
+        self.assertEqual(res, False)
+
+    def test_submission_feedback(self):
+        script.pwd = "xW+5xW+5"
+        res = script.is_valid()
+        self.assertEqual(res, True)
+
+
+
+t = PublicTestSuite()
+t.test_return_types()
+t.test_length()
+t.test_correct_characters()
+t.test_correct_number_of_chars()
+t.test_submission_feedback()
