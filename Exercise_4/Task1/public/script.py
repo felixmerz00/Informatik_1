4100dd6b944792f651cf4a3f79d693b563ae6bef
Felix Merz <felixmerz00@gmail.com>
2022-10-21 18:34:11 +0200

    Recursive and iterative solution pass all the tests. Solution not submitted yet

--- Exercise_4/Task1/public/script.py
+++ Exercise_4/Task1/public/script.py
@@ -2,9 +2,21 @@
 
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
+
+# Recursive solution
 def fac(n):
     # implement this function
-    pass
+    if(n == 0 or n == 1):
+        return 1
+    else:
+        return n * fac(n-1)
+
+# Iterative solution
+def fac_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result = i*result
+    return result
 
 
 # The following line calls the function and prints the return
--- Exercise_4/Task1/public/tests.py
+++ Exercise_4/Task1/public/tests.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 from unittest import TestCase
-from public.script import fac
+from script import fac, fac_iterative
 
 
 class PublicTestSuite(TestCase):
 
     def test1(self):
          self.assertEqual(120, fac(5))
+    
+    def test1_iterative(self):
+        self.assertEqual(120, fac_iterative(5))
+
+t = PublicTestSuite()
+t.test1()
+t.test1_iterative()
 
