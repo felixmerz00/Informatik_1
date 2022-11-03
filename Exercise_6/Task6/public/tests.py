4d8393d9ff16790507d8df9c7bb2b94363237c97
Felix Merz <felixmerz00@gmail.com>
2022-11-03 19:23:58 +0100

    Score: 1/2

--- Exercise_6/Task6/public/tests.py
+++ Exercise_6/Task6/public/tests.py
@@ -19,5 +19,14 @@ class PublicTestSuite(TestCase):
         actual = script.get_possible_nrs("077342119")
         self.assertEqual([], actual, "script should not find any number on an empty wa_nrs list")
 
+    def test_2(self):
+        script.wa_nrs = ["0781111119", "0792653913", "0797763139", "0792793193", "0781139022", "0764320165"]
+        actual = script.get_possible_nrs("076432165")
+        expected = ["0764320165"]
+        self.assertEqual(expected, actual)
+
+    # Hint: The expected phone numbers are ['0779266313', '0789266313', '0792566313', '0792646313', '0792663113', '0792663130', '0792663213', '0792663313', '0792696313', '0796266313'] but ['0779266313', '0789266313', '0792566313', '0792646313', '0792663113', '0792663113', '0792663130', '0792663213', '0792663313', '0792663313', '0792696313', '0796266313'] have been found.
+
 t = PublicTestSuite()
 t.test_1()
+t.test_2()
