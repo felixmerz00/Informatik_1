78263067774408ac26304f2514819eba91ede875
Felix Merz <felixmerz00@gmail.com>
2022-11-03 19:16:36 +0100

    Score: 2/2

--- Exercise_6/Task2/public/script.py
+++ Exercise_6/Task2/public/script.py
@@ -8,33 +8,21 @@ num2 = 28
 # with the **same abundancy** form a **friendly pair.**
 def isFriendlyPair():
     # preconditions: being natural and different from each other
-    if num1 <= 0 or num2 <= 0 or num1 == num2:
+    if num1 <= 0 or num2 <= 0 or num1 == num2 or num1 != round(num1) or num2 != round(num2):
         return "Invalid"
 
-    num1_divisors = []
-    num2_divisors = []
+    sum_divisors1 = num1
+    sum_divisors2 = num2
     for i in range(1, num1):    # Start at 1 to avoid zero division error
         if num1%i == 0:
-            num1_divisors.append(i)
+            sum_divisors1 += i
     for i in range(1, num2):
         if num2%i == 0:
-            num2_divisors.append(i)
-    # a is also a divisor of a, but not included int the list of divisors yet
-    num1_sum_divisors = num1
-    num2_sum_divisors = num2
-    for e in num1_divisors:
-        num1_sum_divisors += e
-    for e in num2_divisors:
-        num2_sum_divisors += e
-    num1_abundancy = num1_sum_divisors/num1
-    num2_abundancy = num2_sum_divisors/num2
+            sum_divisors2 += i
+    abundancy1 = sum_divisors1/num1
+    abundancy2 = sum_divisors2/num2
     # You need to return a boolean variable that sais if num1 and num2 are friendly pairs.
-    return num1_abundancy == num2_abundancy
-    
-    # The numbers must be valid according to description before determining friendly parity situations. 
-    # Return "Invalid" if they are not valid.
-   
-    return True
+    return abundancy1 == abundancy2
 
 #This line prints your method's return so that you can check your output.
-print(isFriendlyPair())
+#print(isFriendlyPair())
--- Exercise_6/Task2/public/tests.py
+++ Exercise_6/Task2/public/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from public import script
+import script
 
 # This test suite only tests whether the given two numbers are
 # friendly pair or not. It uses the numbers 6 and 28, 
@@ -19,3 +19,20 @@ class PublicTestSuite(TestCase):
         actual = script.isFriendlyPair()
         message = "@@{} and {} wrongly detected as not friendly pair even though they are.@@".format(script.num1, script.num2)
         self.assertTrue(actual, message)  
+    
+    def test_num_float(self):
+        script.num1 = 2.6
+        script.num2 = 28
+        actual = script.isFriendlyPair()
+        self.assertEqual("Invalid", actual)
+
+    def test_true(self):
+        script.num1 = 6
+        script.num2 = 28
+        actual = script.isFriendlyPair()
+        self.assertEqual(True, actual)
+
+t = PublicTestSuite()
+t.test_checkFriendly()
+t.test_num_float()
+t.test_true()
