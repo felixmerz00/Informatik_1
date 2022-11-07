35796f0bc6733e512ffa62a98035764018c91964
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-11-07 16:56:06 +0100

    Further setup for midterm

--- Midterm/Programming6/tests.py
+++ Midterm/Programming6/tests.py
@@ -1,2 +1,34 @@
 from unittest import TestCase
 import script
+
+class PublicTestSuite(TestCase):
+
+    def test1(self):
+        actual = True
+        expected = True
+        self.assertEqual(expected, actual)
+        print("Test 1 passed.")
+    
+    def test2(self):
+        actual = True
+        expected = True
+        self.assertEqual(expected, actual)
+        print("Test 2 passed.")
+
+    def test3(self):
+        actual = True
+        expected = True
+        self.assertEqual(expected, actual)
+        print("Test 3 passed.")
+
+    def test4(self):
+        actual = True
+        expected = True
+        self.assertEqual(expected, actual)
+        print("Test 4 passed.")
+
+t = PublicTestSuite()
+# t.test1()
+# t.test2()
+# t.test3()
+# t.test4()
