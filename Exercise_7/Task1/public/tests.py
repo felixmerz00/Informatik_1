68a828a22ad76062a2a60984e45aafcab8c747d9
Felix Merz <felixmerz00@gmail.com>
2022-11-08 16:42:27 +0100

    Task1 solved, not submitted yet.

--- Exercise_7/Task1/public/tests.py
+++ Exercise_7/Task1/public/tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from unittest import TestCase
-from public.script import median
+from script import median
 
 # Extend the test suite with regression tests that cover the
 # meaningful bug reports. Make sure that you define test methods
@@ -23,3 +23,18 @@ class MedianTests(TestCase):
 
     def test_median_works_for_even_lists(self):
         self.assertEqual(3, median([5, 1]))
+
+    def test_median_works_for_even_lists2(self):
+        self.assertEqual(4.5, median([2,3,4,5,6,7]))
+
+    def test_median_works_for_empty_lists(self):
+        self.assertEqual(None, median([]))
+
+t = MedianTests()
+# t.test_median_works_for_single_elements()
+# t.test_median_works_for_sorted_numbers()
+# t.test_median_works_for_unsorted_numbers()
+# t.test_median_works_for_odd_lists()
+# t.test_median_works_for_even_lists()
+t.test_median_works_for_even_lists2()
+t.test_median_works_for_empty_lists()
