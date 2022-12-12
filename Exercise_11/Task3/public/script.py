41570b7b162af303bf94bed169ff4c9f8b4564d6
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-12 15:22:51 +0100

    Task solved, not submitted yet.

--- Exercise_11/Task3/public/script.py
+++ Exercise_11/Task3/public/script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import random
+from difflib import SequenceMatcher
 
 class WordLogic(object):
 
@@ -13,9 +14,23 @@ class WordLogic(object):
             word_list = f.read().splitlines()
         return [word.upper() for word in word_list if len(word) is self.len_words]
 
+
+    def is_similar(self, a, b, threshold):
+        return SequenceMatcher(None, a, b).ratio() > threshold
+
     def word_selection(self):
         words = self.find_words_with_right_size()
         random.shuffle(words)
 
-        # TODO: instead of returning a random sample of words, use the strategy described in task 2
-        return words[0:self.num_words]
+        out_list = words[0:self.num_words // 3]
+        some_word = out_list[0]
+
+        for new_word in words[self.num_words // 3:]:
+
+            if len(out_list) == self.num_words:
+                break
+
+            if self.is_similar(new_word, some_word, 0.4) and new_word not in out_list:
+                out_list.append(new_word)
+
+        return out_list
