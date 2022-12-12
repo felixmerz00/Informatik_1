c4249a5ae0619261546528901af16a0c354d318c
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-12 14:53:35 +0100

    Score: 4/4

--- Exercise_10/Task1/public/bank_account.py
+++ Exercise_10/Task1/public/bank_account.py
@@ -1,19 +1,55 @@
 #!/usr/bin/env python3
 # add imports, if necessary
+from currency_converter import convert
+from exchange_rates import EXCHANGE_RATES
 
 class BankAccount:
 
     def __init__(self, currency="CHF"):
-        pass
+        self.__ALL_CURRENCY = EXCHANGE_RATES.keys()
+        if not currency in self.__ALL_CURRENCY:
+            raise Warning("Given currency does not exist.")
+        self.__currency = currency
+        self.__balance = 0
 
     def get_currency(self):
-        pass
+        return self.__currency
 
     def get_balance(self):
-        pass
+        return self.__balance
         
     def deposit(self, amount, currency="CHF"):
-        pass
+        if not (isinstance(amount,int) or isinstance(amount,float)):
+            raise Warning("Invalid input type for amount.")
+        if not isinstance(currency,str):
+            raise Warning("Invalid input type for currency.")
+        if not currency in self.__ALL_CURRENCY:
+            raise Warning("Given currency does not exist.")
+        if amount <= 0:
+            raise Warning("ValueError")
+        if self.__currency == currency:
+            self.__balance += amount
+        else:
+            self.__balance += convert(amount,currency,self.__currency)
     
     def withdraw(self, amount, currency="CHF"):
-        pass
+        if not (isinstance(amount,int) or isinstance(amount,float)):
+            raise Warning("Invalid input type for amount.")
+        if not isinstance(currency,str):
+            raise Warning("Invalid input type for currency.")
+        if not currency in self.__ALL_CURRENCY:
+            raise Warning("Given currency does not exist.")
+        if amount <=0:
+            raise Warning("ValueError")
+        
+        if currency == self.__currency:
+            if self.__balance < amount:
+                raise Warning("ValueError")
+            else:
+                self.__balance -= amount
+        else:        
+            converted = convert(amount,currency,self.__currency)
+            if self.__balance<converted:
+                raise Warning("ValueError")
+            else:
+                self.__balance -= converted
--- Exercise_10/Task1/public/currency_converter.py
+++ Exercise_10/Task1/public/currency_converter.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 # add imports, if necessary
+from exchange_rates import EXCHANGE_RATES
 
 def convert(amount, from_currency, to_currency):
-    pass
+    if isinstance(amount,int) or isinstance(amount,float):
+        if not (isinstance(from_currency,str) and isinstance(to_currency,str)):
+            raise Warning("Invalid input type for currency.")
+        if from_currency == "" or to_currency == "":
+            raise Warning("ValueError")
+        
+        currencies_list = EXCHANGE_RATES.keys()
+        
+        if from_currency in currencies_list and to_currency in currencies_list:
+            try: 
+                converted = amount * EXCHANGE_RATES[from_currency][to_currency]
+                return converted
+            except:
+                converted = amount / EXCHANGE_RATES[to_currency][from_currency]            
+                return converted
+        else:
+            raise Warning("Invalid currency to convert.")
+
+    else:
+        raise Warning("Invalid input type for amount.")
