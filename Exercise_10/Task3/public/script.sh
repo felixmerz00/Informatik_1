ae9d7e4ef30de35a60a9221ad5ae5bccc8a3eecd
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-12 15:02:38 +0100

    Score: 2/2

--- Exercise_10/Task3/public/script.sh
+++ Exercise_10/Task3/public/script.sh
@@ -11,7 +11,16 @@
 
 git clone $REPO_URL repo
 cd repo
-printf "ccc" > c.txt
-git add c.txt
-git commit -m "Add new file c.txt with some content"
+
+git checkout -b feature_x
+printf 'print("Hello World!")' > hello.py
+git add hello.py
+git commit -m 'Add "Hello World" example'
+git push origin feature_x
+git checkout master
+printf '# good bye' > bye.py
+git add bye.py
+git commit -m 'Add "Good Bye" comment'
+git merge feature_x -m 'Merge "feature_x"'
+
 git push origin master
