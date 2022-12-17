596722e9c2ae616ba18769177c80328c03c8773d
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-17 22:10:38 +0100

    Score: 3/3

--- Exercise_11/Task4/public/game_logic.py
+++ Exercise_11/Task4/public/game_logic.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
 
 # TODO: Implement Me!
+from random import choice
+from abc import ABC, abstractmethod
+
+
+class GameLogic(ABC):
+
+    def __init__(self, num_words, len_words, num_attempts):
+        self.num_words = num_words
+        self.len_words = len_words
+        self.num_attempts = num_attempts
+        self.words = self._word_selection()
+        self.password = choice(self.words)
+
+    def check(self, guess):
+        if self.num_attempts == 0:
+            raise Warning("You have no attempts left.")
+        if len(guess) != self.len_words:
+            return False, ["Input guess has the wrong length"]
+        if guess == self.password:
+            return True, ["Access granted!"]
+        else:
+            return False, [self._generate_feedback(guess), "Access denied!"]
+
+    @abstractmethod
+    def _word_selection(self):
+        pass
+
+    @abstractmethod
+    def _generate_feedback(self, guess):
+        pass
--- Exercise_11/Task4/public/number_logic.py
+++ Exercise_11/Task4/public/number_logic.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python3
 # TODO: Implement Me!
+from public.game_logic import GameLogic
+from random import sample
+
+
+class NumberLogic(GameLogic):
+
+    def _word_selection(self):
+        sequences = []
+        while len(sequences) < self.num_words:
+            sequence = "".join(sample("0123456789", self.len_words))
+            if sequence not in sequences:
+                sequences.append(sequence)
+        return sequences
+
+    def _generate_feedback(self, guess):
+        matching = 0
+        for number in guess:
+            if number in self.password:
+                matching += 1
+        self.num_attempts -= 1
+        return f"{matching}/{self.len_words} correct"
+
+    def check(self, guess):
+        if len(guess) != self.len_words:
+            raise Warning("Input guess has the wrong length")
+        for number in guess:
+            if guess.count(number) != 1:
+                raise Warning("Input can't have duplicate numbers.")
+        return super().check(guess)
